31d7324dffb30a050f2cf54ec1439e7028c5a0b4
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-11-02 23:50:59 +0100

    Create while: warming up.py

---
+++ while: warming up.py
@@ -0,0 +1,27 @@
+"""while: warming up
+The while loop is like a repeated if statement. The code is executed over and over again, as long as the condition 
+is True. Have another look at its recipe.
+
+while condition :
+    expression
+Can you tell how many printouts the following while loop will do?
+
+x = 1
+while x < 4 :
+    print(x)
+    x = x + 1
+Instructions
+
+Possible answers
+- 0
+- 1
+- 2
+- 3
+- 4
+"""
+
+
+x = 1
+while x < 4 :
+    print(x)
+    x = x + 1
