0fa0be975c92758e4c1288a404d51ff5b93c8ca6
Yunus Emre <116467242+yekarayel@users.noreply.github.com>
2023-11-23 23:49:14 +0100

    Create DC_review_practice2.py

---
+++ Rewiev/DC_review_practice2.py
@@ -0,0 +1,14 @@
+"""What is the output of this code? 
+import numpy as np
+m = np.array([6, 2, 4])
+n = 2
+print(m + n)
+
+Select the output
+* [3. 1. 2.]        * [8 4 6]           * [36  4 16]    """
+
+import numpy as np
+m = np.array([6, 2, 4])
+n = 2
+print(m + n)
+
